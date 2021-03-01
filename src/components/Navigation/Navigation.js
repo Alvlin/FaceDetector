97e56351c8322172ad29a548769cc21f1e6142f9
Alvin Lin <Alvinlin425@gmail.com>
2021-03-01 16:51:26 -0500

    update form flow

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Navigation = ({auth,onRouteChange}) => {
-  return(
+  return auth === 'Sign In'
+  ?
+      <div></div>
+  :
     <nav style={{display:'flex', justifyContent:'flex-end'}} onClick={() => onRouteChange('signin','Sign In')}>
       <p className='f3 link dim black underine pa3 pointer'> {auth} </p>
     </nav>
-  );
 }
 
 export default Navigation;
